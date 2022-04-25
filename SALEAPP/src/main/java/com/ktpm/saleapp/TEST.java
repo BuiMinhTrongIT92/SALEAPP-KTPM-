060ca765dbe11c177be642904f3e31bfd06d5d4e
1951052216trong@ou.edu.vn <1951052216trong@ou.edu.vn>
2022-04-25 13:03:41 +0700

    FIXDOUBLE

---
+++ SALEAPP/src/main/java/com/ktpm/saleapp/TEST.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ktpm.saleapp;
+
+import java.io.File;
+
+/**
+ *
+ * @author ACER
+ */
+public class TEST {
+    public static void main(String[] args) {
+        String path = new File("src/main/resources/souresImage").getAbsolutePath();
+        System.out.println(path);
+    }
+}
